cf1dce66df3a04f949879bc2424c3d6d6d011cfb
BlueBigTech <blue.bigtech@gmail.com>
2023-02-24 20:35:44 -0500

    Add Sidebar

--- src/components/LogoBar.js
+++ src/components/LogoBar.js
@@ -1,21 +1,22 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+// import Menu from '@mui/material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import Avatar from '@mui/material/Avatar';
+// import MenuItem from '@mui/material/MenuItem';
 import DehazeIcon from '@mui/icons-material/Dehaze';
 import LogoPng from '../assets/logo.png';
-
-const pages = ['Blog'];
+import { 
+  AppBar,
+  Button,
+  Box,
+  Container,
+  Drawer,
+  IconButton,
+  Tooltip,
+  Toolbar,
+  Typography
+} from '@mui/material';
+const menus = ['UniCred', 'Oceant', 'Events', 'Roadmap', 'About Us!'];
 
 function LogoBar() {
   const [open, setOpen] = React.useState(false);
@@ -23,44 +24,44 @@ function LogoBar() {
   const toggleMenu = () => {
     setOpen(!open);
   };
+  const RightMenu = () => {
+    return (
+      <Box component="div" style={{ width : 300, background : '#161751', height : "100%"}}>
 
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
+      </Box>
+    );
   };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-
 
   return (
     <AppBar position="static" style={{ background : '#161751' }}>
       <Container maxWidth="x2">
         <Toolbar sx={{ justifyContent: "space-between" }}>
-          <IconButton>
-            <img src={LogoPng} style={{ height : 64 }}/>
-            <Typography style={{color : 'white'}}>ElectriCity</Typography>
-          </IconButton>
+          <Tooltip title="Home">
+            <IconButton>
+              <img src={LogoPng} style={{ height : 80 }}/>
+              <Typography style={{color : 'white'}}>ElectriCity</Typography>
+            </IconButton>
+          </Tooltip>
 
           <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'flex' } }}>
-            <Tooltip title="Open settings">
+            <Tooltip title="Shop">
               <Button
                   key="shop"
-                  onClick={handleCloseNavMenu}
+                  // onClick={}
                   sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 Shop
               </Button>
             </Tooltip>
-            <Tooltip title="Open settings">
+            <Tooltip title="Menu">
               <IconButton onClick={toggleMenu} sx={{ p: 1 }}>
                 <DehazeIcon style={{ color : 'white' }}/>
               </IconButton>
             </Tooltip>
           </Box>
+          <Drawer open={open} anchor="right" onClose={toggleMenu}>
+            <RightMenu />
+          </Drawer>
         </Toolbar>
       </Container>
     </AppBar>
